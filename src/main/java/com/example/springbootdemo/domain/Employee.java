53aafe1b0e552f2e06527b6c1aecceb9daf5de89
SHOCKALOVe <SHOCKALOVe@ME>
2019-09-28 15:02:26 +0700

    add domain employee

---
+++ src/main/java/com/example/springbootdemo/domain/Employee.java
@@ -0,0 +1,22 @@
+package com.example.springbootdemo.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee {
+
+    @Id
+   @GeneratedValue
+   @Column(name = "ID")
+   private  Integer id;
+
+   @Column(name = "FIRST_NAME")
+   private String firstName;
+
+   @Column(name = "LAST_NAME")
+   private  String lastName;
+}
